c36a81956e9b0afb800aa0aa3e7a807c4408b5b6
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-07-27 10:38:14 -0500

    Update jenkinsDeploy.groovy

--- jenkinsDeploy.groovy
+++ jenkinsDeploy.groovy
@@ -4,10 +4,14 @@ properties([
         booleanParam(defaultValue: false, description: 'Please select to destroy everything.', name: 'terraformDestroy'),
         booleanParam(defaultValue: false, description: 'Please select to run the job in debug mode', name: 'debugMode'),
         choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please select the environment to deploy.', name: 'environment'),
+        choice(choices: ['us-west-2', 'us-west-1', 'us-east-2', 'us-east-1', 'eu-west-1'], description: 'Please select the region', name: 'aws_region'),
         string(defaultValue: 'None', description: 'Please provide the docker image', name: 'docker_image', trim: true)
         ])
     ])
+
+
 def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+
 def slavePodTemplate = """
       metadata:
         labels:
@@ -53,13 +57,17 @@ def slavePodTemplate = """
             hostPath:
               path: /var/run/docker.sock
     """
+
     podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
       node(k8slabel) {
+          
         stage("Pull SCM") {
             git 'https://github.com/tmoraru/artemis.git'
         }
+
         stage("Generate Variables") {
           dir('deployments/terraform') {
+
             println("Generate Variables")
             def deployment_configuration_tfvars = """
             environment = "${environment}"
@@ -67,47 +75,53 @@ def slavePodTemplate = """
             """.stripIndent()
             writeFile file: 'deployment_configuration.tfvars', text: "${deployment_configuration_tfvars}"
             sh 'cat deployment_configuration.tfvars >> dev.tfvars'
-            sh 'sh /scripts/Dockerfile/set-config.sh'
+
           }   
         }
+
         container("buildtools") {
             dir('deployments/terraform') {
                 withCredentials([usernamePassword(credentialsId: "aws-access-${environment}", 
                     passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
                     println("Selected cred is: aws-access-${environment}")
+                    sh 'sh /scripts/Dockerfile/set-config.sh'
                     stage("Terraform Apply/plan") {
                         if (!params.terraformDestroy) {
                             if (params.terraformApply) {
                                 println("Applying the changes")
                                 sh """
                                 #!/bin/bash
-                                terraform init 
-                                terraform apply -auto-approve
+                                export AWS_DEFAULT_REGION=${aws_region}
+                                source ./setenv.sh dev.tfvars
+                                terraform apply -auto-approve -var-file \$DATAFILE
                                 """
                             } else {
                                 println("Planing the changes")
                                 sh """
                                 #!/bin/bash
-                                set +ex
-                                terraform init 
-                                terraform plan
+                                export AWS_DEFAULT_REGION=${aws_region}
+                                source ./setenv.sh dev.tfvars
+                                terraform plan -var-file \$DATAFILE
                                 """
                             }
                         }
                     }
+
                     stage("Terraform Destroy") {
                         if (params.terraformDestroy) {
                             println("Destroying the all")
                             sh """
                             #!/bin/bash
-                            terraform init 
-                            terraform destroy -auto-approve
+                            export AWS_DEFAULT_REGION=${aws_region}
+                            source ./setenv.sh dev.tfvars
+                            terraform destroy -auto-approve -var-file \$DATAFILE
                             """
                         } else {
                             println("Skiping the destroy")
                         }
                     }
                 }
+
             }
         }
       }
